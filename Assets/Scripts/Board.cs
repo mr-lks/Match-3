8e883f4f20d9a9f33c0a4aa5a1f93fc6cf9f05ba
Ömer Alakuş <alakusomer@gmail.com>
2023-06-13 23:23:34 +0300

    Setup The Board

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class Board : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField]
+    private int width; // The width of the board.
+
+    [SerializeField]
+    private int height; // The height of the board.
+
+    [SerializeField]
+    private GameObject bgTilePrefab; // The prefab for the background tiles.
+
+ 
     void Start()
     {
-        
+        // Setup the board.
+        Setup();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Setup()
     {
+        // Iterate over each tile on the board.
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                // Create a new background tile at the specified position.
+                GameObject bgTile = Instantiate(bgTilePrefab, new Vector2(x, y), Quaternion.identity);
 
-      
+                // Set the tile's parent to the board.
+                bgTile.transform.parent = transform;
 
+                // Set the tile's name.
+                bgTile.name = "BG-Tile [" + x + "," + y + "]";
+            }
+        }
     }
+
 }
